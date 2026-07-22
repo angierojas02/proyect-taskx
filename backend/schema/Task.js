e739c5c3cd9cc9065edff19cc08c1181ef48ee75
angierojas02 <223017952+angierojas02@users.noreply.github.com>
2026-07-21 21:20:43 -0300

    feat(schema): add userId field with validation to task schema

--- backend/schema/Task.js
+++ backend/schema/Task.js
@@ -4,7 +4,11 @@ const taskSchema = z.object({
     title: z.string(),
     description: z.string().optional(),
     priority: z.enum(['Alta', 'Baja', 'Media' ]),
-    status: z.enum(['Pendiente', 'En proceso', 'Completada' ]).default('Pendiente')
+    status: z.enum(['Pendiente', 'En proceso', 'Completada' ]).default('Pendiente'),
+    userId: z
+    .string({ required_error: 'El userId es obligatorio' })
+    .min(1, { message: 'El ID de usuario no es válido' })
+    .regex(/^[0-9a-fA-F]{24}$/, { message: 'El userId debe ser un ObjectId de MongoDB válido' })
 })
 
 export function validateTask (object) {
